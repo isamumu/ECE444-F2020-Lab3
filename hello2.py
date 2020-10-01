810a6515668e3eca543a8675e5cd7654423e7db1
Isamu <isamu.poy@mail.utoronto.ca>
2020-10-01 19:11:34 +0900

    debugged utoronto email error

--- hello2.py
+++ hello2.py
@@ -15,7 +15,7 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
-    email = StringField('What is your UofT Email address?', validators=[Email(), Regexp('^([\w\.\-]+).([\w\.\-]+)@mail.utoronto.ca$', 0, 'must be utoronto email')])
+    email = StringField('What is your UofT Email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,7 +24,7 @@ def index():
     if form.validate_on_submit():
         old_name = session.get('name')
         old_email = session.get('email')
-        print(form.email.data)
+        
 
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
@@ -35,7 +35,7 @@ def index():
         session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form = form, name = session.get('name'), email = session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
